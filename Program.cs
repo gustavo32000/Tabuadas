74b74d719db9ce8b05f3642d963b1f645c2829b5
gugu <gustavocorrea2032@gmail.com>
2019-07-22 09:58:04 -0300

    Criada tabuada do 2 ao 10

--- Program.cs
+++ Program.cs
@@ -6,7 +6,16 @@ namespace Tabuadas
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Clear();
+            for (int numero=2 ; numero<=10 ;numero++){
+                for (int x=0 ; x<=10 ; x++){
+                    Console.WriteLine(numero + " X " + x + " = " + (numero*x));
+                }
+                Console.WriteLine();
+                Console.WriteLine("--------------------------");
+                Console.WriteLine();
+
+            }
         }
     }
 }
