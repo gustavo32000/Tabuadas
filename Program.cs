c4f364a81eacc697b4abad92c9822f9d70632d2b
gugu <gustavocorrea2032@gmail.com>
2019-07-22 09:24:37 -0300

    Criação do Projeto

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tabuadas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
